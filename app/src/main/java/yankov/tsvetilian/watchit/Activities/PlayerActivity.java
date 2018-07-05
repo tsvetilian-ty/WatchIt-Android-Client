bf36f0ad4034ea10e78747bf1fc9af33e9edad1e
Tsvetilian Yankov <tsvetilian.ty@gmail.com>
2018-07-05 11:32:01 +0300

    add exo player to the player activity

--- app/src/main/java/yankov/tsvetilian/watchit/Activities/PlayerActivity.java
+++ app/src/main/java/yankov/tsvetilian/watchit/Activities/PlayerActivity.java
@@ -3,12 +3,25 @@ package yankov.tsvetilian.watchit.Activities;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.squareup.picasso.Picasso;
 
 import yankov.tsvetilian.watchit.Models.WatchModel;
 import yankov.tsvetilian.watchit.R;
@@ -17,6 +30,8 @@ import yankov.tsvetilian.watchit.ViewModels.WatchViewModel;
 public class PlayerActivity extends AppCompatActivity {
 
     private WatchViewModel watchViewModel;
+    private SimpleExoPlayer player;
+    private PlayerView playerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +40,14 @@ public class PlayerActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_player);
         watchViewModel = ViewModelProviders.of(this).get(WatchViewModel.class);
+        playerView = findViewById(R.id.player);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        NotificationManagerCompat.from(this).cancel(110);
+
         Intent playerData = getIntent();
         if (playerData.getExtras() != null && playerData.hasExtra("id")) {
             watchViewModel.getWatchById(playerData.getExtras().getInt("id")).observe(this, new Observer<WatchModel>() {
@@ -38,7 +56,7 @@ public class PlayerActivity extends AppCompatActivity {
                     if (watch != null) {
                         watch.setWatched(true);
                         watchViewModel.update(watch);
-                        Log.d("WATCHIT", "FROM ACTIVITY");
+                        playInit(watch);
                     }
                 }
             });
@@ -46,7 +64,7 @@ public class PlayerActivity extends AppCompatActivity {
             WatchModel watch = playerData.getExtras().getParcelable("watch");
             watch.setWatched(true);
             watchViewModel.insert(watch);
-            Log.d("WATCHIT", "FROM SERVICE");
+            playInit(watch);
         } else if (playerData.getExtras() != null && playerData.hasExtra("later")) {
             WatchModel watch = playerData.getExtras().getParcelable("later");
             watch.setWatchLater(true);
@@ -55,8 +73,29 @@ public class PlayerActivity extends AppCompatActivity {
         }
     }
 
+    private void playInit(WatchModel watch) {
+        player = ExoPlayerFactory.newSimpleInstance(PlayerActivity.this, new DefaultTrackSelector());
+
+        playerView.setPlayer(player);
+
+        DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory(Util.getUserAgent(PlayerActivity.this, "WatchIT"), null);
+        dataSourceFactory.createDataSource().setRequestProperty("Authorization", String.format("Bearer %s", watch.getAuthKey()));
+
+        Uri url = Uri.parse(String.format("http://%s:3090/watch%s/%s", watch.getSourceIp(), watch.getPath(), watch.getFileName()));
+
+        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(url);
+
+        player.prepare(mediaSource);
+        player.setPlayWhenReady(true);
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
+        playerView.setPlayer(null);
+        if (player != null) {
+            player.release();
+            player = null;
+        }
     }
 }
